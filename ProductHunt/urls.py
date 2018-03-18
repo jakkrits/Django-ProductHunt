db60a6a11cafebb6ba55b506bbb0d4ec4030af3f
JakkritS <jakk.sutt@gmail.com>
2018-03-19 02:24:07 +0700

    add product

--- ProductHunt/urls.py
+++ ProductHunt/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 import products.views
-import accounts.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.home, name='home'),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls'))
 ]
--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    url = models.URLField()
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField()
+    image = models.ImageField(upload_to='images/products')
+    icon = models.ImageField(upload_to='images/icon')
+    body = models.TextField(max_length=500)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def summary(self):
+        return self.body[:100]
+
+
