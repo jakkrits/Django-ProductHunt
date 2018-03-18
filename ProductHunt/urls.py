ccd3af784455da9593ba32bd973667eb05273fde
JakkritS <jakk.sutt@gmail.com>
2018-03-18 22:05:47 +0700

    add auth

--- ProductHunt/urls.py
+++ ProductHunt/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import products.views
+import accounts.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', products.views.home, name='home')
+    path('', products.views.home, name='home'),
+    path('accounts/', accounts.views.home, name='accounts')
 ]
